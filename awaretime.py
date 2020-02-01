11021db22bb06f9d828688311ea92295bf65a351
Emilio Araos <ea3dev@gmail.com>
2020-01-31 18:40:47 -0700

    time challenge

--- awaretime.py
+++ awaretime.py
@@ -17,5 +17,17 @@ gap_time = datetime.datetime(2020, 1, 31, 0, 0)
 print(gap_time)
 print(gap_time.timestamp())
 
+s = 1445733000
+t = s + (60 * 60)
+
+gb = pytz.timezone('GB')
+dt1 = pytz.utc.localize(datetime.datetime.utcfromtimestamp(s)).astimezone(gb)
+dt2 = pytz.utc.localize(datetime.datetime.utcfromtimestamp(t)).astimezone(gb)
+
+print("{} seconds since the epoch is {}".format(s, dt1))
+print("{} seconds since the epoch is {}".format(t, dt2))
+
+
+
 
 
---
+++ timechallenge.py
@@ -0,0 +1,13 @@
+# Create a program that allows  a user to choose one of
+# up to 9 time zones from a menu. You can choose any
+# zones you want from the all_timezones lists.
+
+# The program will then display the time in that time zone as
+# well as local time and UTC time.
+# Entering 0 as the choice will quit the program
+
+# Display the dates and times in a format suitable for the
+# user of your program to understand, and include the
+# timezone name then displaying the chosen line
+
+
